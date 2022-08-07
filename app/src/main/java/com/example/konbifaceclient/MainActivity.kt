f710dd482bc08d232434fa6f92f469af2f05e1a6
Saikiran <psk.knr@gmail.com>
2022-08-07 14:26:48 +0530

    Activity Result Launcher Added

--- app/src/main/java/com/example/konbifaceclient/MainActivity.kt
+++ app/src/main/java/com/example/konbifaceclient/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.konbifaceclient
 
+import android.app.Activity
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
@@ -14,10 +17,18 @@ class MainActivity : AppCompatActivity() {
         val clickMe = findViewById<AppCompatButton>(R.id.click_me)
         clickMe.setOnClickListener {
             val intent = Intent()
-            intent.component =
-                ComponentName("com.example.konbiface", "com.example.konbiface.MainActivity")
-            startActivity(intent)
+            intent.component = ComponentName("com.example.konbiface", "com.example.konbiface.MainActivity")
+            getResult.launch(intent)
         }
-
     }
+
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val value = it.data?.getStringExtra("input")
+                Log.i("Saikiran", value.toString())
+            }
+        }
 }
